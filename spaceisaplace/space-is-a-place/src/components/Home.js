5767cc82a394f32904503ee3a50b3e63ddc91cf3
PearlCxndy <pearlcxndie@gmail.com>
2024-03-24 02:46:07 +0000

    clean up

--- spaceisaplace/space-is-a-place/src/components/Home.js
+++ spaceisaplace/space-is-a-place/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { ScrollControls, Scroll, Sparkles, Backdrop, Float, Ring, useScroll } from '@react-three/drei';
+import { ScrollControls, Scroll, Sparkles, Backdrop, Float, Ring } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import transition from "../transition";
 import DraggableSpace from './line.js';
